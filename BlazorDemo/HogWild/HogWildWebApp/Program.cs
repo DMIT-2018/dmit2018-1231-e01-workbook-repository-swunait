3f61e3e91af0901177475232d9ed9cd1732a46cc
Sam Wu <swu@nait.ca>
2023-10-27 21:01:37 -0600

    Started Seession 4

--- BlazorDemo/HogWild/HogWildWebApp/Program.cs
+++ BlazorDemo/HogWild/HogWildWebApp/Program.cs
@@ -2,7 +2,31 @@ using HogWildWebApp.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
+using HogWildSystem;                    // for AddBackendDependencies extension method
+using Microsoft.EntityFrameworkCore;    // for UseSqlServer extension extension method
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//  :given (This is code that is provided when we create our application)
+//  supplied database connection due to the fact that we created this
+//      web app to use Individual accounts
+
+//  :added
+//  code retrieves the HogWild connection string
+var connectionStringHogWild = builder.Configuration.GetConnectionString("OLTP-DMIT2018");
+
+//  added:
+//  Code the logic to add our class library services to IServiceCollection
+//  One could do the registration code here in Program.cs
+//  HOWEVER, every time a service class is added, you would be changing this file
+//  The implementation of the DBContent and AddTransient(...) code in this example
+//        will be done in an extension method to IServiceCollection
+//  The extension method will be code inside the HogWildSystem class library
+//  The extension method will have a paramater: options.UseSqlServer()
+builder.Services.AddBackendDependencies(options =>
+    options.UseSqlServer(connectionStringHogWild));
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
