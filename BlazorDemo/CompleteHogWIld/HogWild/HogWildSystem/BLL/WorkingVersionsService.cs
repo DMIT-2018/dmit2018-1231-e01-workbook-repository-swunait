3a6b95f937edee83369feaf3ba01dd831050e525
Sam Wu <swu@nait.ca>
2023-11-17 20:44:10 -0700

    added completed HogWild app

---
+++ BlazorDemo/CompleteHogWIld/HogWild/HogWildSystem/BLL/WorkingVersionsService.cs
@@ -0,0 +1,41 @@
+﻿#nullable disable
+using HogWildSystem.DAL;
+using HogWildSystem.ViewModels;
+
+namespace HogWildSystem.BLL
+{
+    public class WorkingVersionsService
+    {
+        #region Fields
+
+        /// <summary>
+        /// The hog wild context
+        /// </summary>
+        private readonly HogWildContext _hogWildContext;
+
+        #endregion
+
+        // Constructor for the WorkingVersionsService class.
+        internal WorkingVersionsService(HogWildContext hogWildContext)
+        {
+            // Initialize the _hogWildContext field with the provided HogWildContext instance.
+            _hogWildContext = hogWildContext;
+        }
+
+        // This method retrieves the working version of a resource.
+        public WorkingVersionsView GetWorkingVersion()
+        {
+            return _hogWildContext.WorkingVersions
+                .Select(x => new WorkingVersionsView
+                {
+                    VersionId = x.VersionId,
+                    Major = x.Major,
+                    Minor = x.Minor,
+                    Build = x.Build,
+                    Revision = x.Revision,
+                    AsOfDate = x.AsOfDate,
+                    Comments = x.Comments
+                }).FirstOrDefault();
+        }
+    }
+}
---
+++ BlazorDemo/CompleteHogWIld/HogWild/HogWildWebApp/Components/DialogConfirm.razor.cs
@@ -0,0 +1,65 @@
+﻿// ***********************************************************************
+// Assembly         : HogWildWebApp
+// Author           : James Thompson
+// Created          : 06-19-2023
+//
+// Last Modified By : James Thompson
+// Last Modified On : 06-20-2023
+// ***********************************************************************
+// <copyright file="DialogConfirm.razor.cs" company="NAIT">
+//     Copyright (c) Northern Alberta Institute of Technology. All rights reserved.
+// </copyright>
+// <summary>
+//  Components using with MubBlazor 
+// </summary>
+// ***********************************************************************
+
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace HogWildWebApp.Components
+{
+    /// <summary>
+    /// Class DialogConfirm.
+    /// </summary>
+    public partial class DialogConfirm : IDisposable
+    {
+        /// <summary>
+        /// Gets or sets the mud dialog.
+        /// </summary>
+        //  The mud dialog
+        [CascadingParameter] MudDialogInstance MudDialog { get; set; }
+
+        /// <summary>
+        /// Gets or sets the content text.
+        /// </summary>
+        //  The content text
+        [Parameter] public string ContentText { get; set; }
+
+        /// <summary>
+        /// Gets or sets the button text.
+        /// </summary>
+        //  The button text
+        [Parameter] public string ButtonText { get; set; }
+
+        /// <summary>
+        /// Gets or sets the color.
+        /// </summary>
+        //  The color
+        [Parameter] public Color Color { get; set; }
+
+        /// <summary>
+        /// Submits this instance.
+        /// </summary>
+        private void Submit() => MudDialog.Close(DialogResult.Ok(true));
+        /// <summary>
+        /// Cancels this instance.
+        /// </summary>
+        private void Cancel() => MudDialog.Cancel();
+
+        public void Dispose()
+        {
+         //   throw new NotImplementedException();
+        }
+    }
+}
